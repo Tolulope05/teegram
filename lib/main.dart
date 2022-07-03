28da4875ccaea869ea7a456cefe2103415d926e2
Tolulope Fakunle <fakunletolulope05@gmail.com>
2022-07-03 23:51:45 +0100

    added Firebase Sdk and enabled MulitiDesk

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import './responsive/mobile_screen_layout.dart';
 import './responsive/responsive_layout_screen.dart';
 import './responsive/web_screen_layout.dart';
 import './utils/colors.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
