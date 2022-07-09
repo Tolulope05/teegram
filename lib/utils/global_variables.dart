31fa26a755e6dd5131aa778609925836c3b5d963
Tolulope Fakunle <fakunletolulope05@gmail.com>
2022-07-09 18:38:44 +0100

    added static post and feed screen waiting for dynamic firebase data

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:teegram/screens/add_post_screen.dart';
+import '../screens/add_post_screen.dart';
+import '../screens/feed_screen.dart';
 
 const webScreenSize = 600;
 
 List<Widget> homeScreenItems = [
-  const Text('feed'),
+  const FeedScreen(),
   const Text('search'),
   const AddPostScreen(),
   const Text('notif'),
