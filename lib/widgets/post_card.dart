a45bb6112dd754607f74e8b787e14983d3b60482
Tolulope Fakunle <fakunletolulope05@gmail.com>
2022-07-12 12:11:02 +0100

    displayed comment length in post card..

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -6,6 +7,7 @@ import 'package:teegram/providers/user_provider.dart';
 import 'package:teegram/resources/firestore_methods.dart';
 import 'package:teegram/screens/comments_screen.dart';
 import 'package:teegram/utils/colors.dart';
+import 'package:teegram/utils/utils.dart';
 import 'package:teegram/widgets/like_animation.dart';
 
 class PostCard extends StatefulWidget {
@@ -18,6 +20,28 @@ class PostCard extends StatefulWidget {
 
 class _PostCardState extends State<PostCard> {
   bool isLikeAnimating = false;
+  int commentLength = 0;
+  @override
+  void initState() {
+    super.initState();
+    getcomments();
+  }
+
+  void getcomments() async {
+    try {
+      QuerySnapshot snap = await FirebaseFirestore.instance
+          .collection("posts")
+          .doc(widget.snap["postId"])
+          .collection("comment")
+          .get();
+
+      commentLength = snap.docs.length;
+    } catch (e) {
+      showSnackBar(e.toString(), context);
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     final User user = Provider.of<UserProvider>(context).getUser;
@@ -97,7 +121,7 @@ class _PostCardState extends State<PostCard> {
               alignment: Alignment.center,
               children: [
                 SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.4,
+                  height: MediaQuery.of(context).size.height * 0.55,
                   width: double.infinity,
                   child: Image.network(
                     widget.snap['postUrl'],
@@ -106,7 +130,8 @@ class _PostCardState extends State<PostCard> {
                           ? child
                           : const Center(child: CircularProgressIndicator());
                     },
-                    fit: BoxFit.cover,
+                    fit: BoxFit.fitWidth,
+                    alignment: Alignment.center,
                   ),
                 ),
                 AnimatedOpacity(
@@ -219,9 +244,10 @@ class _PostCardState extends State<PostCard> {
                   onTap: () {},
                   child: Container(
                     padding: const EdgeInsets.symmetric(vertical: 4),
-                    child: const Text(
-                      'View all 200 comments',
-                      style: TextStyle(fontSize: 16, color: secondaryColor),
+                    child: Text(
+                      'View all $commentLength comments',
+                      style:
+                          const TextStyle(fontSize: 16, color: secondaryColor),
                     ),
                   ),
                 ),
